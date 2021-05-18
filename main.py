71688a72e42ef0e5c47e8c8adff5f142477952ab
EHAT32 <enatik14@gmail.com>
2021-05-18 23:31:26 +0300

    Final version

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import numpy as np
 import math
 
+#The first part of the lab
+
 def first_eq(x):
     f = x * np.log(x + 1) - 0.3
     return f
@@ -9,11 +11,15 @@ def first_eq_deriv(x):
     d = np.log(x + 1) + (x / (x + 1))
     return d
 
+#locator of the roots
+
 def locate_root(x_l, x_r, dx_l, dx_r):
     while first_eq(x_l) * first_eq(x_r) > 0:
         x_l += dx_l
         x_r += dx_r
     return x_l, x_r
+
+#Newton's method to find roots of the equation
 
 def solver_1(x_0):
     solution = x_0 - first_eq(x_0) / first_eq_deriv(x_0)
@@ -22,11 +28,17 @@ def solver_1(x_0):
         solution = x_0 - first_eq(x_0) / first_eq_deriv(x_0) 
     return solution
 
-def jacob(X):
+#The second part of the lab
+
+#Simple Jacobian
+
+def jacobian(X):
     return np.array([
         [2, math.cos(X[1])],
         [-math.sin(X[0] - 1), 1]
     ])
+
+#Nonlinear system of equations
 
 def nonlinear_system(X):
     return np.array([
@@ -34,13 +46,15 @@ def nonlinear_system(X):
         X[1] + math.cos(X[0] - 1) - 0.7
     ])
 
+#Newton's method to solve nonlinear system
+
 def solver_2(X):
-    j_inv = np.linalg.inv(jacob(X))
+    j_inv = np.linalg.inv(jacobian(X))
     f = nonlinear_system(X)
     solution = X - np.matmul(j_inv, f)
     while np.linalg.norm(X - solution) > 1e-4:
         X = solution
-        solution = X - np.matmul(np.linalg.inv(jacob(X)), nonlinear_system(X)) 
+        solution = X - np.matmul(np.linalg.inv(jacobian(X)), nonlinear_system(X)) 
     return solution
 
 def main():
@@ -49,11 +63,12 @@ def main():
     root_1 = solver_1(x_1_r)
     root_2 = solver_1(x_2_l)
     if abs(root_1) > abs(root_2):
-        print(root_2)
+        print('1. Least modulo root: {:.4f}'.format(root_2))
     else:
-        print(root_1)
-    
-    print(solver_2(np.zeros((2, 1))))
+        print('1. Least modulo root: {:.4f}'.format(root_1))
+        
+    np.set_printoptions(formatter={'float_kind':'{:.4f}'.format})
+    print('2. Solution for nonlinear system: ', solver_2(np.zeros((2, 1)))) #The first number is x-coordinate, the second is y-coordinate
     return
 
 if __name__ == '__main__':
